650ad380ff0909477b33e1766bea2987e287462a
SimoL-003 <simoneleuzzi2003@gmail.com>
2025-12-14 15:48:48 +0100

    first commit

---
+++ data/posts.js
@@ -0,0 +1,37 @@
+const posts = [
+  {
+    titolo: "Ciambellone",
+    caption: "Dolce soffice e classico, perfetto per colazione o merenda.",
+    tags: ["dolce", "colazione", "soffice", "vegetariano"],
+    immagine: "images/posts/ciambellone.jpeg",
+  },
+  {
+    titolo: "Cracker alla barbabietola",
+    caption:
+      "Sfiziosi cracker croccanti con barbabietola, ideali per aperitivi o snack.",
+    tags: ["salato", "snack", "aperitivo", "vegetariano", "barbabietola"],
+    immagine: "images/posts/cracker_barbabietola.jpeg",
+  },
+  {
+    titolo: "Pane fritto dolce",
+    caption:
+      "Fette di pane fritte e zuccherate, una coccola golosa per ogni momento della giornata.",
+    tags: ["dolce", "fritto", "merenda", "vegetariano"],
+    immagine: "images/posts/pane_fritto_dolce.jpeg",
+  },
+  {
+    titolo: "Pasta alla barbabietola",
+    caption: "Pasta con crema di barbabietola, un piatto colorato e saporito.",
+    tags: ["primo", "vegetariano", "barbabietola", "colorato"],
+    immagine: "images/posts/pasta_barbabietola.jpeg",
+  },
+  {
+    titolo: "Torta paesana",
+    caption:
+      "Torta rustica e ricca di sapori tradizionali, tipica della cucina contadina.",
+    tags: ["dolce", "rustico", "tradizionale", "vegetariano"],
+    immagine: "images/posts/torta_paesana.jpeg",
+  },
+];
+
+export { posts };
